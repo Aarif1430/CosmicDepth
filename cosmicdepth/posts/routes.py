a16a56f0a6559fe07c68759e6134c5627adb9445
Aarif1430 <malikarif13@gmail.com>
2020-11-22 12:04:55 +0000

    changed root folder name to cosmicdepth

--- cosmicdepth/posts/routes.py
+++ cosmicdepth/posts/routes.py
@@ -1,9 +1,9 @@
 from flask import (render_template, url_for, flash,
                    redirect, request, abort, Blueprint)
 from flask_login import current_user, login_required
-from flaskblog import db
-from flaskblog.models import Post
-from flaskblog.posts.forms import PostForm
+from cosmicdepth import db
+from cosmicdepth.models import Post
+from cosmicdepth.posts.forms import PostForm
 
 posts = Blueprint('posts', __name__)
 
